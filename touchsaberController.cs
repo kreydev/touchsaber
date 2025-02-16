548b915254d8e4d9a85c0f151be8d484cea530fb
KreyDev <quackface2@gmail.com>
2025-02-15 20:37:32 -0500

    fix memory leaks, theoretically

--- touchsaberController.cs
+++ touchsaberController.cs
@@ -1,22 +1,11 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 using UnityEngine;
 using static UnityEngine.Input;
 using UnityEngine.UI;
 using UnityEngine.EventSystems;
-using UnityEngine.SceneManagement;
-using System.Runtime.CompilerServices;
 using VRUIControls;
-using System.Reflection;
-using System.IO;
 
-
-public enum ctrlMode { touch, keybmouse, controller, arrows}
+public enum ctrlMode { touch, keybmouse, controller, arrows, chaos}
 
 namespace touchsaber
 {
@@ -34,8 +23,10 @@ namespace touchsaber
         //GameObject cube;
         int s1 = 0;
         int s2 = 1;
+        Vector2 arrows = new Vector2();
         List<Saber> sabers2 = new List<Saber>();
         Saber[] sabers;
+        float saberDistance = 1;
 
         // These methods are automatically called by Unity, you should remove any you aren't using.
         #region Monobehaviour Messages
@@ -78,13 +69,183 @@ namespace touchsaber
                 goodcam.CopyFrom(GameObject.Find("Cam").GetComponent<Camera>());
 
             }
-            //foreach (Saber s in sabers) {
-            //    Debug.Log(s.name);
-            //    print("zawg");
-            //    Application.Quit();
-            //}
         }
+
+        void DoCameraShit()
+        {
+            if (!GetKey(KeyCode.F1) && !GetKey(KeyCode.F2))
+            {
+                goodcam.transform.position = new Vector3(0.00f, 1.20f, -2.20f);
+            }
+
+            Vector2 arrows = new Vector2(bi.GetAxisRaw("Horizontal"), bi.GetAxisRaw("Vertical"));
+            if (GetKey(KeyCode.F2))
+            {
+                goodcam.transform.localEulerAngles += new Vector3(-arrows.y * 3f, arrows.x * 3f, 0);
+            }
+            else if (!GetKey(KeyCode.F1))
+            {
+                goodcam.transform.rotation = Quaternion.identity;
+            }
+
+            sabers = FindObjectsOfType<Saber>();
+            sabers2.Clear();
+            foreach (Saber saber in sabers)
+            {
+                if (Vector3.Distance(saber.transform.position, goodcam.transform.position) < 4)
+                {
+                    sabers2.Add(saber);
+                }
+            }
+        }
+
         void Update() {
+            arrows.x = bi.GetAxisRaw("Horizontal");
+            arrows.y = bi.GetAxisRaw("Vertical");
+            if (saberDistance < 3 / .2) {
+                saberDistance += mouseScrollDelta.y / 3;
+            }
+            if (!GetKey(KeyCode.F1) && !GetKey(KeyCode.F2))
+            {
+                goodcam.transform.position = new Vector3(0.00f, 1.20f, -2.20f);
+            }
+
+            if (GetKey(KeyCode.F2))
+            {
+                goodcam.transform.localEulerAngles += new Vector3(-arrows.y * 3f, arrows.x * 3f, 0);
+            }
+            else if (!GetKey(KeyCode.F1))
+            {
+                goodcam.transform.rotation = Quaternion.identity;
+            }
+
+            sabers = FindObjectsOfType<Saber>();
+            sabers2.Clear();
+            foreach (Saber saber in sabers)
+            {
+                if (Vector3.Distance(saber.transform.position, goodcam.transform.position) < 4)
+                {
+                    sabers2.Add(saber);
+                }
+            }
+
+            DoCameraShit();
+
+            if (sabers2.Count > 0)
+            {
+                // touchscreen
+                if (Plugin.mode == ctrlMode.touch)
+                {
+                    for (int i = 0; i < bi.touchCount; i++)
+                    {
+                        if (i < 2)
+                        {
+                            sabers2[i].transform.LookAt(goodcam.transform);
+                            sabers2[i].transform.Rotate(180, 180, 180);
+                            sabers2[i].transform.eulerAngles = new Vector3((bi.GetTouch(i).position.y - Screen.height / 3) * -.2f, (bi.GetTouch(i).position.x - Screen.width / 2) * .1f, 0);
+                            sabers2[i].transform.position = goodcam.ScreenToWorldPoint(new Vector3(bi.GetTouch(i).position.x, bi.GetTouch(i).position.y, -goodcam.transform.position.z)) + new Vector3(0, 0, .2f);
+                            sabers2[i].transform.localPosition += new Vector3(0, 0, -.5f);
+                        }
+                    }
+                }
+
+                // touchscreen + chaos
+                if (Plugin.mode == ctrlMode.chaos)
+                {
+                    for (int i = 0; i < bi.touchCount; i++)
+                    {
+                        if (i == sabers2.Count) {
+                            sabers2.Add(Instantiate(sabers2[i-1]));
+                            print(sabers2[i].saberType);
+                        }
+                        sabers2[i].transform.LookAt(goodcam.transform);
+                        sabers2[i].transform.Rotate(180, 180, 180);
+                        sabers2[i].transform.eulerAngles = new Vector3((bi.GetTouch(i).position.y - Screen.height / 3) * -.2f, (bi.GetTouch(i).position.x - Screen.width / 2) * .1f, 0);
+                        sabers2[i].transform.position = goodcam.ScreenToWorldPoint(new Vector3(bi.GetTouch(i).position.x, bi.GetTouch(i).position.y, -goodcam.transform.position.z)) + new Vector3(0, 0, .2f);
+                        sabers2[i].transform.localPosition += new Vector3(0, 0, -.5f);
+                    }
+                }
+
+                //arrow keys
+                else if (Plugin.mode == ctrlMode.arrows)
+                {
+                    sabers2[0].transform.rotation = Quaternion.Slerp(sabers2[0].transform.rotation, Quaternion.Euler(new Vector3(-arrows.y * 60, arrows.x * 60, 0)), .5f);
+                    sabers2[0].transform.position = goodcam.transform.position + new Vector3(.2f, -.5f, .5f);
+                    sabers2[1].transform.rotation = Quaternion.Slerp(sabers2[1].transform.rotation, Quaternion.Euler(new Vector3(-arrows.y * 60, arrows.x * 60, 0)), .5f);
+                    sabers2[1].transform.position = goodcam.transform.position + new Vector3(-.2f, -.5f, .5f);
+                }
+
+                // just mouse + keyboard
+                else if (Plugin.mode == ctrlMode.keybmouse)
+                {
+                    Cursor.lockState = CursorLockMode.None;
+                    Cursor.visible = true;
+                    if (GetKey(KeyCode.S))
+                    {
+                        s2 = 0;
+                        s1 = 1;
+                    }
+                    else
+                    {
+                        s2 = 1;
+                        s1 = 0;
+                    }
+                    sabers2[s1].transform.LookAt(goodcam.transform);
+                    sabers2[s1].transform.Rotate(180, 180, 180);
+                    if (sabers2.Count != 1)
+                    {
+                        sabers2[s2].transform.LookAt(goodcam.transform);
+                        sabers2[s2].transform.Rotate(180, 180, 180);
+                    }
+                    if (GetKey(KeyCode.A) && GetKey(KeyCode.D))
+                    {
+                        sabers2[s1].transform.eulerAngles = new Vector3((bi.mousePosition.y - Screen.height / 3) * -.2f, (bi.mousePosition.x - 1 * Screen.width / 2) * .1f + 50f, 0);
+                        sabers2[s1].transform.position = goodcam.ScreenToWorldPoint(new Vector3(bi.mousePosition.x, bi.mousePosition.y, -goodcam.transform.position.z)) + new Vector3(1f, 0, .2f);
+
+                        if (sabers2.Count != 1)
+                        {
+                            sabers2[s2].transform.eulerAngles = new Vector3((bi.mousePosition.y - Screen.height / 3) * -.2f, (bi.mousePosition.x - 1 * Screen.width / 2) * .1f - 50f, 0);
+                            sabers2[s2].transform.position = goodcam.ScreenToWorldPoint(new Vector3(bi.mousePosition.x, bi.mousePosition.y, -goodcam.transform.position.z)) + new Vector3(-1f, 0, .2f);
+                        }
+                    }
+                    else if (GetKey(KeyCode.A))
+                    {
+                        sabers2[s1].transform.eulerAngles = new Vector3((bi.mousePosition.y - Screen.height / 3) * -.2f, (bi.mousePosition.x - 1 * Screen.width / 2) * .1f, 0);
+                        sabers2[s1].transform.position = goodcam.ScreenToWorldPoint(new Vector3(bi.mousePosition.x, bi.mousePosition.y, -goodcam.transform.position.z)) + new Vector3(0, .2f, .2f);
+
+                        if (sabers2.Count != 1)
+                        {
+                            sabers2[s2].transform.eulerAngles = new Vector3((bi.mousePosition.y - Screen.height / 3) * -.2f, (bi.mousePosition.x - 1 * Screen.width / 2) * .1f, 0);
+                            sabers2[s2].transform.position = goodcam.ScreenToWorldPoint(new Vector3(bi.mousePosition.x, bi.mousePosition.y, -goodcam.transform.position.z)) + new Vector3(0, -.2f, .2f);
+                        }
+                    }
+                    else if (GetKey(KeyCode.D))
+                    {
+                        sabers2[s2].transform.eulerAngles = new Vector3((bi.mousePosition.y - Screen.height / 3) * -.2f, (bi.mousePosition.x - Screen.width / 2) * .1f, 0);
+                        sabers2[s2].transform.position = goodcam.ScreenToWorldPoint(new Vector3(bi.mousePosition.x, bi.mousePosition.y, -goodcam.transform.position.z)) + new Vector3(0, .2f, .2f);
+
+                        if (sabers2.Count != 1)
+                        {
+                            sabers2[s1].transform.eulerAngles = new Vector3((bi.mousePosition.y - Screen.height / 3) * -.2f, (bi.mousePosition.x - Screen.width / 2) * .1f, 0);
+                            sabers2[s1].transform.position = goodcam.ScreenToWorldPoint(new Vector3(bi.mousePosition.x, bi.mousePosition.y, -goodcam.transform.position.z)) + new Vector3(0, -.2f, .2f);
+                        }
+                    }
+                    else
+                    {
+                        sabers2[s1].transform.eulerAngles = new Vector3((bi.mousePosition.y - Screen.height / 3) * -.2f, (bi.mousePosition.x - Screen.width / 2) * .1f, 0);
+                        sabers2[s1].transform.position = goodcam.ScreenToWorldPoint(new Vector3(bi.mousePosition.x, bi.mousePosition.y, -goodcam.transform.position.z)) + new Vector3(.2f * saberDistance, 0, .2f);
+
+                        if (sabers2.Count != 1)
+                        {
+                            sabers2[s2].transform.eulerAngles = new Vector3((bi.mousePosition.y - Screen.height / 3) * -.2f, (bi.mousePosition.x - Screen.width / 2) * .1f, 0);
+                            sabers2[s2].transform.position = goodcam.ScreenToWorldPoint(new Vector3(bi.mousePosition.x, bi.mousePosition.y, -goodcam.transform.position.z)) + new Vector3(-.2f * saberDistance, 0, .2f);
+                        }
+                    }
+                }
+            }
+        }
+
+        void UpdateOld() {
             //if (cube == null) {
             //    cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
             //    cube.name = "holder";
@@ -100,24 +261,7 @@ namespace touchsaber
             //        //ctrl.transform.position = goodcam.ScreenToWorldPoint(new Vector3(bi.mousePosition.x, bi.mousePosition.y, -goodcam.transform.position.z));
             //    }
             //}
-            if (!UnityEngine.Input.GetKey(KeyCode.F1) && !UnityEngine.Input.GetKey(KeyCode.F2)) {
-                goodcam.transform.position = new Vector3(0.00f, 1.20f, -2.20f);
-            }
-
-            Vector2 arrows = new Vector2(bi.GetAxisRaw("Horizontal"), bi.GetAxisRaw("Vertical"));
-            if (UnityEngine.Input.GetKey(KeyCode.F2)) {
-                goodcam.transform.localEulerAngles += new Vector3(-arrows.y * 3f, arrows.x * 3f, 0);
-            } else if (!UnityEngine.Input.GetKey(KeyCode.F1)) {
-                goodcam.transform.rotation = Quaternion.identity;
-            }
-
-            sabers = FindObjectsOfType<Saber>();
-            sabers2.Clear();
-            foreach (Saber saber in sabers) {
-                if (Vector3.Distance(saber.transform.position, goodcam.transform.position) < 4) {
-                    sabers2.Add(saber);
-                }
-            }
+            DoCameraShit();
 
             string objs = "---===---\n";
             foreach (GameObject g in FindObjectsOfType<GameObject>())
@@ -126,23 +270,23 @@ namespace touchsaber
             }
             objs += "---===---";
 
-            if (UnityEngine.Input.GetKeyDown(KeyCode.Escape))
+            if (GetKeyDown(KeyCode.Escape))
             {
                 StandardLevelReturnToMenuController pauser = FindObjectOfType<StandardLevelReturnToMenuController>();
                 pauser.ReturnToMenu();
             }
 
-            if (UnityEngine.Input.GetKeyDown(KeyCode.C)) {
+            if (GetKeyDown(KeyCode.C)) {
                 Debug.Log("SABERS 2: " + sabers2.Count);
                 Debug.Log("SABERS: " + sabers.Length);
             }
 
-            if (sabers2.Count == 0 && UnityEngine.Input.GetKey(KeyCode.F1) && !UnityEngine.Input.GetKey(KeyCode.F2)) {
-                goodcam.transform.position += new Vector3(0, arrows.y * .2f, arrows.x * .2f);
+            if (sabers2.Count == 0 && GetKey(KeyCode.F1) && !GetKey(KeyCode.F2)) {
+                goodcam.transform.Translate(0, arrows.y * .2f, arrows.x * .2f);
             }
 
             if (sabers2.Count > 0) {
-                // arrow keys
+                //arrow keys
                 if (Plugin.mode == ctrlMode.arrows)
                 {
                     sabers2[0].transform.rotation = Quaternion.Slerp(sabers2[0].transform.rotation, Quaternion.Euler(new Vector3(-arrows.y * 60, arrows.x * 60, 0)), .5f);
@@ -179,6 +323,8 @@ namespace touchsaber
 
                 // just mouse + keyboard
                 else if (Plugin.mode == ctrlMode.keybmouse) {
+                    Cursor.lockState = CursorLockMode.None;
+                    Cursor.visible = true;
                     if (GetKey(KeyCode.S))
                     {
                         s2 = 0;
